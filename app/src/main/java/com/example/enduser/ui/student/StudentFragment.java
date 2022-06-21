dc155956c8467ab8b97ef186252aade51837e5bb
AnkitaZade03 <ankitazade2003@gmail.com>
2022-06-21 16:22:08 +0530

    Home

--- app/src/main/java/com/example/enduser/ui/student/StudentFragment.java
+++ app/src/main/java/com/example/enduser/ui/student/StudentFragment.java
@@ -13,7 +13,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import com.example.enduser.Activity.Customer;
 import com.example.enduser.Activity.User;
 import com.example.enduser.Adapter.StudentAdapter;
 import com.example.enduser.R;
